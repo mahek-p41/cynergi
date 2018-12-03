66d9d94f00b4467c8a32e46a21b1800909fddf38
garym <garym@hightouchinc.com>
2018-12-03 10:58:03 -0600

    Got the flyway migration working correctly

---
+++ src/main/kotlin/com/hightouchinc/cynergi/middleware/data/access/AddressDataAccessObject.kt
@@ -0,0 +1,8 @@
+package com.hightouchinc.cynergi.middleware.data.access
+
+import javax.inject.Singleton
+
+@Singleton
+class AddressDataAccessObject {
+
+}
--- src/main/resources/db/migration/postgres/V1__initial.sql
+++ src/main/resources/db/migration/postgres/V1__initial.sql
@@ -11,7 +11,10 @@ CREATE TABLE address(
   state                  VARCHAR (2)                           NOT NULL,
   postal_code            VARCHAR (20)                          NOT NULL,
   phone                  VARCHAR (20)                          NOT NULL,
-  fax                    VARCHAR (20)                          NOT NULL
+  fax                    VARCHAR (20)                          NOT NULL,
+  work_phone             VARCHAR (20)                          NOT NULL,
+  work_phone_ext         VARCHAR (10)                          NOT NULL,
+  map_cd                 VARCHAR (20)                          NOT NULL
 );
 CREATE TABLE business(
   id                     BIGSERIAL                             NOT NULL PRIMARY KEY,
@@ -29,3 +32,13 @@ CREATE TABLE store(
   address_id             BIGINT REFERENCES address (id)        NOT NULL,
   business_id            BIGINT REFERENCES business (id)       NOT NULL
 );
+
+CREATE TABLE customer(
+  id                     BIGSERIAL                             NOT NULL PRIMARY KEY,
+  UUID                   UUID      DEFAULT uuid_generate_v1()  NOT NULL,
+  date_created           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  last_updated           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  last_name              VARCHAR(150) NOT NULL,
+  first_name             VARCHAR(150) NOT NULL,
+  address                BIGINT REFERENCES address (id)        NOT NULL
+)
