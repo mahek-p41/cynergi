779599e03a05f6e76ee69ab4b1fd10a529feaa46
garym <garym@hightouchinc.com>
2018-12-03 16:00:32 -0600

    Added more tables

--- src/main/resources/db/migration/postgres/V1__initial.sql
+++ src/main/resources/db/migration/postgres/V1__initial.sql
@@ -41,4 +41,31 @@ CREATE TABLE customer(
   last_name              VARCHAR(150) NOT NULL,
   first_name             VARCHAR(150) NOT NULL,
   address                BIGINT REFERENCES address (id)        NOT NULL
+);
+
+CREATE TABLE automobile ( -- care_infos
+  id                     BIGSERIAL                             NOT NULL PRIMARY KEY,
+  UUID                   UUID      DEFAULT uuid_generate_v1()  NOT NULL,
+  date_created           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  last_updated           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  license                VARCHAR(50)                           NOT NULL,
+  year                   NUMBER(4)                             NOT NULL,
+  make                   VARCHAR(50)                           NOT NULL,
+  model                  VARCHAR(50)                           NOT NULL,
+  description_color      VARCHAR(50)                           NOT NULL,
+  vin                    VARCHAR (150)                         NOT NULL,
+  lien_holder            VARCHAR (150),
+  lien_phone             VARCHAR(20),
+  payment_amount         NUMBER(19, 2),
+  payment_frequency      VARCHAR(150),
+  purchase_date          DATE                                  NOT NULL,
+  customer_id            BIGINT REFERENCES customer (id)       NOT NULL
+);
+
+CREATE TABLE customer_collections (
+  id                     BIGSERIAL                             NOT NULL PRIMARY KEY,
+  UUID                   UUID      DEFAULT uuid_generate_v1()  NOT NULL,
+  date_created           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  last_updated           TIMESTAMP DEFAULT current_timestamp   NOT NULL,
+  customer_account_id    BIGINT REFERENCES customer (id)       NOT NULL
 )
