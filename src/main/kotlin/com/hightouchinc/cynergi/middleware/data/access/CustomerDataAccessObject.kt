2edb15e9e2525dede1d8871511a30d3fc5b192a2
garym <garym@hightouchinc.com>
2018-12-10 17:35:26 -0600

    Began work on the customer search query

--- src/main/kotlin/com/hightouchinc/cynergi/middleware/data/access/CustomerDataAccessObject.kt
+++ src/main/kotlin/com/hightouchinc/cynergi/middleware/data/access/CustomerDataAccessObject.kt
@@ -3,6 +3,7 @@ package com.hightouchinc.cynergi.middleware.data.access
 import com.hightouchinc.cynergi.middleware.data.domain.Page
 import com.hightouchinc.cynergi.middleware.data.transfer.CustomerDataTransferObject
 import org.springframework.jdbc.core.JdbcTemplate
+import java.sql.ResultSet
 import javax.inject.Singleton
 
 @Singleton
@@ -10,6 +11,10 @@ class CustomerDataAccessObject(
    private val jdbcTemplate: JdbcTemplate
 ){
    fun searchForCustomers(customerSearchString: String): Page<CustomerDataTransferObject> {
-      val content = jdbcTemplate.
+      /*val content = jdbcTemplate.query("") { rs: ResultSet ->
+
+      }*/
+
+      return Page(listOf(), 0, 1, false, false)
    }
 }
