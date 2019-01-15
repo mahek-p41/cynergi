4027d4eee3d16240fe34454a2a35d172ebf5162b
garym <garym@hightouchinc.com>
2019-01-15 13:03:00 -0600

    Began work on the test data loader for the checklist employment entity

---
+++ src/test/groovy/com/hightouchinc/cynergi/test/data/loader/ChecklistEmployment.groovy
@@ -0,0 +1,11 @@
+package com.hightouchinc.cynergi.test.data.loader
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ChecklistEmploymentTestDataLoader {
+}
+
+class ChecklistEmploymentDataLoaderService {
+
+}
