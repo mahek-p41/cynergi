3222864587fe0703c15f1af43bfc6d46d7e24ec1
garym <garym@hightouchinc.com>
2019-01-24 10:51:39 -0600

    Added flyway-migration for the notification_recipient table into cynergidb

---
+++ src/main/resources/db/migration/postgres/V3__feature-notificaton-recipient-table-cyn294.sql
@@ -0,0 +1,14 @@
+CREATE TABLE notification_recipient (
+   id              BIGSERIAL                              NOT NULL PRIMARY KEY,
+   uu_row_id       UUID        DEFAULT uuid_generate_v1() NOT NULL,
+   time_created    TIMESTAMPTZ DEFAULT clock_timestamp()  NOT NULL,
+   time_updated    TIMESTAMPTZ DEFAULT clock_timestamp()  NOT NULL,
+   description     VARCHAR(255),
+   recipient       VARCHAR(255)                           NOT NULL,
+   notification_id BIGINT REFERENCES notification(id)     NOT NULL
+);
+CREATE TRIGGER update_notification_recipient_trg
+   BEFORE UPDATE
+   ON notification_recipient
+   FOR EACH ROW
+EXECUTE PROCEDURE last_updated_column_fn();
