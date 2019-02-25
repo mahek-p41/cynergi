1be3d25dcc191d971589993ca4716c835e9debdb
garym <garym@hightouchinc.com>
2019-02-25 12:26:08 -0600

    Added some Or null methods to the ResultSetExtensions

--- src/main/kotlin/com/hightouchinc/cynergi/middleware/extensions/ResultSetExtensions.kt
+++ src/main/kotlin/com/hightouchinc/cynergi/middleware/extensions/ResultSetExtensions.kt
@@ -5,6 +5,20 @@ import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 
-fun ResultSet.getOffsetDateTime(column: String) : OffsetDateTime = this.getObject(column, OffsetDateTime::class.java)
-fun ResultSet.getLocalDate(column: String) : LocalDate = this.getObject(column, LocalDate::class.java)
-fun ResultSet.getUUID(column: String) : UUID = this.getObject(column, UUID::class.java)
+fun ResultSet.getOffsetDateTime(column: String) : OffsetDateTime =
+   this.getObject(column, OffsetDateTime::class.java)
+
+fun ResultSet.getOffsetDateTimeOrNull(column: String) : OffsetDateTime? =
+   this.getObject(column, OffsetDateTime::class.java)
+
+fun ResultSet.getLocalDate(column: String) : LocalDate =
+   this.getObject(column, LocalDate::class.java)
+
+fun ResultSet.getLocalDateOrNull(column: String) : LocalDate =
+   this.getObject(column, LocalDate::class.java)
+
+fun ResultSet.getUUID(column: String) : UUID =
+   this.getObject(column, UUID::class.java)
+
+fun ResultSet.getUUIDOrNull(column: String) : UUID? =
+   this.getObject(column, UUID::class.java)
