bf1e24d97b9cbe2722aa1f5d83b0e03e5e3101ee
garym <garym@hightouchinc.com>
2019-03-01 17:55:07 -0600

    Added jwt auth via cookie that doesn't work

---
+++ src/main/kotlin/com/hightouchinc/cynergi/middleware/authentication/AuthenticationService.kt
@@ -0,0 +1,39 @@
+package com.hightouchinc.cynergi.middleware.authentication
+
+import io.micronaut.security.authentication.AuthenticationFailed
+import io.micronaut.security.authentication.AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH
+import io.micronaut.security.authentication.AuthenticationResponse
+import io.reactiverse.reactivex.pgclient.PgPool
+import io.reactiverse.reactivex.pgclient.Tuple
+import io.reactivex.Single
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthenticationService @Inject constructor(
+   private val client: PgPool
+) {
+   private val logger: Logger = LoggerFactory.getLogger(AuthenticationService::class.java)
+
+   fun authenticate(username: String, password: String): Single<AuthenticationResponse> {
+      logger.debug("Attempting authentication for user {}", username)
+
+      return client.rxPreparedQuery("SELECT level FROM employee WHERE username = $1 AND password = $2 LIMIT 1", Tuple.of(username, password)).map { rs ->
+         val iterator = rs.iterator()
+
+         if(iterator.hasNext()) {
+            logger.trace("successfully authenticated user {}", username)
+
+            val row = iterator.next()
+
+            AuthenticatedUserDetails(userName = username, level = row.getInteger("level"), userRoles = emptySet())
+         } else {
+            logger.trace("Unable to authenticate user {}", username)
+
+            AuthenticationFailed(CREDENTIALS_DO_NOT_MATCH)
+         }
+      }
+   }
+}
--- src/main/kotlin/com/hightouchinc/cynergi/middleware/authentication/OperatorAuthenticationProvider.kt
+++ src/main/kotlin/com/hightouchinc/cynergi/middleware/authentication/OperatorAuthenticationProvider.kt
@@ -4,11 +4,20 @@ import io.micronaut.security.authentication.AuthenticationProvider
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
 import org.reactivestreams.Publisher
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class OperatorAuthenticationProvider : AuthenticationProvider {
-   override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
-         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class OperatorAuthenticationProvider @Inject constructor(
+   private val authenticationService: AuthenticationService
+) : AuthenticationProvider {
+   private val logger: Logger = LoggerFactory.getLogger(OperatorAuthenticationProvider::class.java)
+
+   override fun authenticate(authenticationRequest: AuthenticationRequest<in String, in String>): Publisher<AuthenticationResponse> {
+      logger.debug("Authentication requested {}/{}", authenticationRequest.identity, authenticationRequest.secret)
+
+      return authenticationService.authenticate(username = authenticationRequest.identity as String, password = authenticationRequest.secret as String).toFlowable()
    }
 }
