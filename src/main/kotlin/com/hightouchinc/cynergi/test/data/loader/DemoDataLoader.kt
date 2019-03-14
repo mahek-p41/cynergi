aeeda56b326c39ad45449e0c395f20b12e73cd7d
garym <garym@hightouchinc.com>
2019-03-14 10:35:17 -0500

    Added AP modules with company associations and desired levels to demo data

--- src/main/kotlin/com/hightouchinc/cynergi/test/data/loader/DemoDataLoader.kt
+++ src/main/kotlin/com/hightouchinc/cynergi/test/data/loader/DemoDataLoader.kt
@@ -1,6 +1,7 @@
 package com.hightouchinc.cynergi.test.data.loader
 
 import com.hightouchinc.cynergi.middleware.repository.AreaRepository
+import com.hightouchinc.cynergi.middleware.repository.CompanyModuleAccessRepository
 import com.hightouchinc.cynergi.middleware.repository.MenuRepository
 import com.hightouchinc.cynergi.middleware.repository.ModuleRepository
 import io.micronaut.context.annotation.Requires
@@ -15,6 +16,7 @@ import javax.inject.Singleton
 @Requires(env = ["demo", "test"])
 class DemoDataLoader @Inject constructor(
    private val areaRepository: AreaRepository,
+   private val companyModuleAccessDataLoaderService: CompanyModuleAccessDataLoaderService,
    private val companyDataLoaderService: CompanyDataLoaderService,
    private val menuRepository: MenuRepository,
    private val moduleRepository: ModuleRepository,
@@ -32,19 +34,55 @@ class DemoDataLoader @Inject constructor(
       val apMenu = menuRepository.findOne("AP")!!
       val apModules = moduleRepository.findAllAssociatedWithMenu(apMenu)
       val apArea = areaRepository.associate(apMenu, company, 2)
-      // TODO associate modules to company with proper level
+      val apCompanyModules = companyModuleAccessDataLoaderService.associate(companyIn = company, moduleLevels = listOf(
+         "APADD" to 10,
+         "APCHG" to 30,
+         "APDEL" to 30,
+         "APSTATUS" to 10,
+         "APUTIL" to 10,
+         "APRECUR" to 10,
+         "APGLRPT" to 40,
+         "APCHKLST" to 20,
+         "APLST" to 10,
+         "APCHECK" to 40,
+         "APREPORT" to 10,
+         "APSEL" to 30,
+         "APSELDUE" to 30,
+         "APSHO" to 10,
+         "APVOID" to 60,
+         "PRTAPDST" to 0
+      ))
 
       logger.info("Setting up APRECR for {}", company)
       val aprecurMenu = menuRepository.findOne("APRECUR")!!
       val aprecurModules = moduleRepository.findAllAssociatedWithMenu(aprecurMenu)
       val aprecurArea = areaRepository.associate(aprecurMenu, company, 2)
-      // TODO associate modules to company with proper level
+      val aprecurCompanyModules = companyModuleAccessDataLoaderService.associate(companyIn = company, moduleLevels = listOf(
+         "ADDAPREC" to 10,
+         "CHGAPREC" to 30,
+         "DELAPREC" to 40,
+         "LSTAPREC" to 10,
+         "PRTAPREC" to 20,
+         "SHOAPREC" to 10,
+         "TRNAPREC" to 30
+      ))
 
       logger.info("Setting up APREPORT for {}", company)
       val apreportMenu = menuRepository.findOne("APREPORT")!!
       val apreportModules = moduleRepository.findAllAssociatedWithMenu(apreportMenu)
       val apreportArea = areaRepository.associate(aprecurMenu, company, 2)
-      // TODO associate modules to company with proper level
+      val apreportCompanyModules = companyModuleAccessDataLoaderService.associate(companyIn = company, moduleLevels = listOf(
+         "APAGERPT" to 10,
+         "FLOWANAL" to 10,
+         "APCHKRPT" to 20,
+         "APEXPENS" to 20,
+         "APRPT" to 20,
+         "APPREVUE" to 30,
+         "CASHOUT" to 20,
+         "APTRLBAL" to 30,
+         "AP1099" to 20,
+         "APUNDO" to 10
+      ))
 
 
       logger.info("Finished creating demo data")
