a4a4052c68a96eefec206b044eab409e90c46e60
garym <garym@hightouchinc.com>
2019-05-09 11:58:59 -0500

    Fixed some casing problems with the migration templates
    Added some init sql to the test database to get it into a state that is ready for testing that will be provided by external patching

--- support/docker/db/cynergi-initdb.sql
+++ support/docker/db/cynergi-initdb.sql
@@ -1,32 +1,45 @@
 \c fastinfo_production
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE OR REPLACE VIEW employee AS
-   SELECT emp_nbr AS number,
+   SELECT
+      id AS id,
+      ht_etl_checksum::uuid AS uu_row_id,
+      created_at AT TIME ZONE 'UTC' AS time_created,
+      updated_at AT TIME ZONE 'UTC' AS time_updated,
+      emp_nbr AS number,
       TRIM(BOTH FROM
-           CAST(emp_pass_1 AS TEXT) ||
-           CAST(emp_pass_2 AS TEXT) ||
-           CAST(emp_pass_3 AS TEXT) ||
-           CAST(emp_pass_4 AS TEXT) ||
-           CAST(emp_pass_5 AS TEXT) ||
-           CAST(emp_pass_6 AS TEXT)
-          ) AS passcode
+         CAST(emp_pass_1 AS TEXT) ||
+         CAST(emp_pass_2 AS TEXT) ||
+         CAST(emp_pass_3 AS TEXT) ||
+         CAST(emp_pass_4 AS TEXT) ||
+         CAST(emp_pass_5 AS TEXT) ||
+         CAST(emp_pass_6 AS TEXT)
+      ) AS passcode,
+      true AS active
    FROM corrto.level1_loc_emps
    UNION ALL
-   SELECT emp_nbr AS number,
+   SELECT
+      id AS id,
+      ht_etl_checksum::uuid AS uu_row_id,
+      created_at AT TIME ZONE 'UTC' AS time_created,
+      updated_at AT TIME ZONE 'UTC' AS time_updated,
+      emp_nbr AS number,
       TRIM(BOTH FROM
-           CAST(emp_pass_1 AS TEXT) ||
-           CAST(emp_pass_2 AS TEXT) ||
-           CAST(emp_pass_3 AS TEXT) ||
-           CAST(emp_pass_4 AS TEXT) ||
-           CAST(emp_pass_5 AS TEXT) ||
-           CAST(emp_pass_6 AS TEXT)
-          ) AS passcode
+         CAST(emp_pass_1 AS TEXT) ||
+         CAST(emp_pass_2 AS TEXT) ||
+         CAST(emp_pass_3 AS TEXT) ||
+         CAST(emp_pass_4 AS TEXT) ||
+         CAST(emp_pass_5 AS TEXT) ||
+         CAST(emp_pass_6 AS TEXT)
+      ) AS passcode,
+      true AS active
    FROM corrnr.level1_loc_emps
    ORDER BY number;
 
 \c cynergidb
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 CREATE SCHEMA fastinfo_prod_import;
-CREATE SCHEMA cyn_temp;
 
 CREATE SERVER fastinfo
    FOREIGN DATA WRAPPER postgres_fdw
