b0a2d6b1a3ad51d5b6c2e46246e92b0b3cdd174a
Brian Ayres <briana@hightouchinc.com>
2019-08-07 16:06:23 -0500

    Added V8 table definition file for schedules

---
+++ src/main/resources/db/migration/postgres/V8__feature-schedule-tables.sql
@@ -0,0 +1,27 @@
+--- begin schedule type domain table
+CREATE TABLE schedule_type_domain (
+   id                INTEGER                                NOT NULL PRIMARY KEY,
+   value             VARCHAR(15)                            NOT NULL,
+   description       VARCHAR(50)                            NOT NULL,
+   localization_code VARCHAR(50)                            NOT NULL
+);
+CREATE UNIQUE INDEX schedule_type_domain_value ON schedule_type_domain (value);
+
+INSERT INTO schedule_type_domain(id, value, description, localization_code) VALUES (1, 'CRON', 'Cron style schedule', 'schedule.cron');
+
+CREATE TABLE schedule (
+   id                BIGSERIAL                                             NOT NULL PRIMARY KEY,
+   uu_row_id         UUID           DEFAULT uuid_generate_v1()             NOT NULL,
+   time_created      TIMESTAMPTZ    DEFAULT clock_timestamp()              NOT NULL,
+   time_updated      TIMESTAMPTZ    DEFAULT clock_timestamp()              NOT NULL,
+   title             VARCHAR(64)                                           NOT NULL,
+   description       VARCHAR(256),
+   schedule          VARCHAR(592)                                          NOT NULL,
+   command           VARCHAR(1024)                                         NOT NULL,
+   type_id           INTEGER        REFERENCES schedule_type_domain (id)   NOT NULL,
+);
+CREATE TRIGGER update_schedule_trg
+   BEFORE UPDATE
+   ON schedule
+   FOR EACH ROW
+EXECUTE PROCEDURE last_updated_column_fn();
