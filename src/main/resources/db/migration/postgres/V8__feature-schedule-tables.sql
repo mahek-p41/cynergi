b42c4d6919c979b2b388b9c7ac7f012482ebe896
Brian Ayres <briana@hightouchinc.com>
2019-08-20 15:13:15 -0500

    Updated the schedule tables file to include an argument table.

--- src/main/resources/db/migration/postgres/V8__feature-schedule-tables.sql
+++ src/main/resources/db/migration/postgres/V8__feature-schedule-tables.sql
@@ -9,6 +9,17 @@ CREATE UNIQUE INDEX schedule_type_domain_value ON schedule_type_domain (value);
 
 INSERT INTO schedule_type_domain(id, value, description, localization_code) VALUES (1, 'CRON', 'Cron style schedule', 'schedule.cron');
 
+CREATE TABLE schedule_arg (
+    id INTEGER NOT NULL PRIMARY KEY,
+    value VARCHAR(256) NOT NULL,
+    description VARCHAR(256)
+);
+CREATE TRIGGER update_schedule_arg_trg
+   BEFORE UPDATE
+   ON schedule_arg
+   FOR EACH ROW
+EXECUTE PROCEDURE last_updated_column_fn();
+
 CREATE TABLE schedule (
    id                BIGSERIAL                                             NOT NULL PRIMARY KEY,
    uu_row_id         UUID           DEFAULT uuid_generate_v1()             NOT NULL,
@@ -18,7 +29,8 @@ CREATE TABLE schedule (
    description       VARCHAR(256),
    schedule          VARCHAR(592)                                          NOT NULL,
    command           VARCHAR(1024)                                         NOT NULL,
-   type_id           INTEGER        REFERENCES schedule_type_domain (id)   NOT NULL
+   type_id           INTEGER        REFERENCES schedule_type_domain (id)   NOT NULL,
+   schedule_arg_id   INTEGER        REFERENCES schedule_arg (id)           NULL
 );
 CREATE TRIGGER update_schedule_trg
    BEFORE UPDATE
