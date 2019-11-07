e41e8786f7a5cd5663ffb04ef7d4ee832b3bd528
garym <garym@hightouchinc.com>
2019-11-07 12:51:53 -0600

    altered docker host db to use the setup-database.sql that is deployed with the production build

--- support/development/db/cynergi-initdb.sh
+++ support/development/db/cynergi-initdb.sh
@@ -7,6 +7,10 @@ createdb cynergidevelopdb
 
 dropdb --if-exists cynergidb
 createdb cynergidb
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "cynergidb" <<-EOSQL
+   CREATE USER cynergiuser WITH PASSWORD 'password';
+   ALTER ROLE cynergiuser SUPERUSER;
+EOSQL
 
 if [[ -f /tmp/dumps/cynergidb.dump ]]; then
     echo "Restoring cynergidb from snapshot"
@@ -22,3 +26,5 @@ if [[ -f /tmp/dumps/fastinfo.dump ]]; then
     pg_restore -v -O -x --role=postgres --dbname=fastinfo_production /tmp/dumps/fastinfo.dump
     echo "Finished restoring fastinfo_production from snapshot"
 fi
+
+psql -f /tmp/setup-database.sql -v "ON_ERROR_STOP=1" -v fastinfoUserName=postgres -v fastinfoPassword=password -v datasets=corrto
