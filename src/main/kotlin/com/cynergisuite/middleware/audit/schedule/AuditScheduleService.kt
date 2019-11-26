9a2cd64b0f284aa366bcb6f45cf7d9899bed085e
Monte McGuire <montem@hightouchinc.com>
2019-11-26 09:07:39 -0600

    Unstaged

--- src/main/kotlin/com/cynergisuite/middleware/audit/schedule/AuditScheduleService.kt
+++ src/main/kotlin/com/cynergisuite/middleware/audit/schedule/AuditScheduleService.kt
@@ -114,26 +114,27 @@ class AuditScheduleService @Inject constructor(
       )
    }
 
-//   private fun createNotificationAndAudit(schedule: ScheduleEntity) {
-//      val stores = mutableListOf<StoreValueObject>()
-//      val notificationDomainType = notificationTypeDomainRepository.findOne("S")!!
-//
-//      for (arg: ScheduleArgumentEntity in schedule.arguments) {
-//         if (arg.description == "storeNumber") {
-//            val store = storeRepository.findOneByNumber(arg.value.toInt())!!
-//
-//            stores.add(StoreValueObject(store))
-//            val xx = companyRepository.findCompanyByStore(store)
-//            val oneNote = Notification(startDate = LocalDate.now(),
-//                                       expirationDate = LocalDate.now().plusDays(7),
-//                                       message = schedule.description!!,
-//                                       sendingEmployee = "todo",
-//                                       company = "todo2",
-//                                       notificationDomainType = notificationDomainType)
-//
-//            xxx = notificationRepository.insert(note)
-//         }
-//      }
-//
-//   }
+   private fun createNotificationAndAudit(schedule: ScheduleEntity) {
+      val stores = mutableListOf<StoreValueObject>()
+      val notificationDomainType = notificationTypeDomainRepository.findOne("S")!!
+
+      for (arg: ScheduleArgumentEntity in schedule.arguments) {
+         if (arg.description == "storeNumber") {
+            val store = storeRepository.findOneByNumber(arg.value.toInt())!!
+
+            stores.add(StoreValueObject(store))
+            var companyName = companyRepository.findCompanyByStore(store)?.name
+            val xx = companyRepository.findCompanyByStore(store)
+            val oneNote = Notification(startDate = LocalDate.now(),
+                                       expirationDate = LocalDate.now().plusDays(7),
+                                       message = schedule.description!!,
+                                       sendingEmployee = "todo",
+                                       company = companyName!!,
+                                       notificationDomainType = notificationDomainType)
+
+            xxx = notificationRepository.insert(note)
+         }
+      }
+
+   }
 }
