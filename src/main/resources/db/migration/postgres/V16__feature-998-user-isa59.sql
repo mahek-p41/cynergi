bda5bb68ee4ae93df0b3d22af0556ad6829c52f3
garym <garym@hightouchinc.com>
2019-12-03 14:36:08 -0600

    Dropped unique constraint on employee.pass_code

--- src/main/resources/db/migration/postgres/V16__feature-998-user-isa59.sql
+++ src/main/resources/db/migration/postgres/V16__feature-998-user-isa59.sql
@@ -9,3 +9,5 @@ UPDATE employee
 SET pass_code = '$2a$10$s62wpFGlz2iJ3yCzhgnof.6d7RQ8BZLesaKVzoo5YtneqYXUNFBvO'
 WHERE number = 998;
 
+ALTER TABLE employee
+    DROP CONSTRAINT employee_number_pass_code_key;
