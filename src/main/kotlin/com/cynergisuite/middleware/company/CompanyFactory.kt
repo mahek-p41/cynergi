3076228d9f387609564c10598db2f1f30d41a743
Monte McGuire <montem@hightouchinc.com>
2020-01-20 17:01:13 -0600

    Started adding company table info from Beckys create info

--- src/main/kotlin/com/cynergisuite/middleware/company/CompanyFactory.kt
+++ src/main/kotlin/com/cynergisuite/middleware/company/CompanyFactory.kt
@@ -8,12 +8,23 @@ object CompanyFactory {
 
    @JvmStatic
    private val companies: List<CompanyEntity> = listOf(
-      CompanyEntity(id = 4, number = 0, name = "RENTAL CITY",  dataset = "tstds1"),
-      CompanyEntity(id = 1, number = 0, name = "Pelham Trading Post, Inc.", dataset = "tstds2")
+      CompanyEntity(
+         id = 4,
+         name = "RENTAL CITY",
+         doingBusinessAs = null!!,
+         clientCode = "RCT",
+         clientId = 1234,
+         datasetCode = "tstds1",
+         federalTaxNumber = "A1000B200"),
+      CompanyEntity(
+         id = 1,
+         name = "Pelham Trading Post, Inc.",
+         doingBusinessAs = "RentACenter",
+         clientCode = "PTP",
+         clientId = 4321,
+         datasetCode = "tstds2",
+         federalTaxNumber = "BX101010")
    )
-
-   @JvmStatic
-   fun random() = companies.random()
 
    @JvmStatic
    fun all(): List<CompanyEntity> = companies
@@ -25,9 +36,9 @@ class CompanyFactoryService(
    private val companyRepository: CompanyRepository
 ) {
 
-   fun forDataset(dataset: String): CompanyEntity =
-      companyRepository.findByDataset(dataset) ?: throw Exception("Unable to find Company $dataset")
+   fun forDatasetCode(datasetCode: String): CompanyEntity =
+      companyRepository.findByDataset(datasetCode) ?: throw Exception("Unable to find Company $datasetCode")
 
    fun random(): CompanyEntity =
-      forDataset(CompanyFactory.random().dataset)
+      forDatasetCode(CompanyFactory.random().datasetCode)
 }
