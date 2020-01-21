c990adbf7585d9349180618a95cbeaf18835d6aa
garym <garym@hightouchinc.com>
2020-01-21 16:57:14 -0600

    Made some tweaks to company create table statement

--- src/main/resources/db/migration/postgres/V23__feature-insert-company.sql
+++ src/main/resources/db/migration/postgres/V23__feature-insert-company.sql
@@ -1,18 +1,17 @@
-CREATE TABLE company (
-    id                BIGSERIAL                                          NOT NULL PRIMARY KEY,
-    UUID              UUID        DEFAULT uuid_generate_v1()             NOT NULL,
-    date_created      TIMESTAMPTZ DEFAULT clock_timestamp()              NOT NULL,
-    last_updated      TIMESTAMPTZ DEFAULT clock_timestamp()              NOT NULL,
-    name              VARCHAR(150) CHECK ( char_length(name) > 1 )       NOT NULL,
-    doing_business_as VARCHAR(100) CHECK ( char_length(doing_business_as) > 1 ),
-    client_code       VARCHAR(6) CHECK ( char_length(client_code) > 1 )  NOT NULL,
-    client_id         INTEGER CHECK ( client_id > 0 )                    NOT NULL,
-    dataset_code      VARCHAR(6) CHECK ( char_length(dataset_code) = 6 ) NOT NULL,
-    fin               VARCHAR(12)
+CREATE TABLE company
+(
+    id                 BIGSERIAL                                                NOT NULL PRIMARY KEY,
+    uu_row_id          UUID        DEFAULT uuid_generate_v1()                   NOT NULL,
+    time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                    NOT NULL,
+    time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                    NOT NULL,
+    name               VARCHAR(150) CHECK ( char_length(trim(name)) > 1 )       NOT NULL,
+    doing_business_as  VARCHAR(100) CHECK ( char_length(trim(doing_business_as)) > 1 ),
+    client_code        VARCHAR(6) CHECK ( char_length(trim(client_code)) > 1 )  NOT NULL,
+    client_id          INTEGER CHECK ( client_id > 0 )                          NOT NULL,
+    dataset_code       VARCHAR(6) CHECK ( char_length(trim(dataset_code)) = 6 ) NOT NULL,
+    federal_tax_number VARCHAR(12),
+    UNIQUE (client_id)
 );
-
-CREATE UNIQUE INDEX company_client_id_index ON company (client_id);
-
 CREATE TRIGGER update_company_trg
     BEFORE UPDATE
     ON company
