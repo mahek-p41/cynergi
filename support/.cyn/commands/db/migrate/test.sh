23772f9a393a0dfbc171a66835265c0bc5af99b8
garym <garym@hightouchinc.com>
2020-01-26 22:08:56 -0600

    Added cyn scripting tools for migrating the databases

---
+++ support/.cyn/commands/db/migrate/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+## description: migrate via flyway the cynergitestdb database
+
+cd ../development
+
+if [ -z `docker-compose ps -q cynergitestdb` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q cynergitestdb)` ]; then
+  echo "cynergitestdb is not running! Not connecting psql."
+  exit 1
+else
+  echo "Migration Test database"
+  cd ../../
+  pwd
+  ./gradlew flywayMigrateCynergiTestDb
+  exit $?
+fi
+
