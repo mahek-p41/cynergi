e4df28b672c1625498d5923236791f092b756632
garym <garym@hightouchinc.com>
2020-01-30 17:38:04 -0600

    Fixed Inventory listing and audit creation to only use inventory that is active at a store where location and primary location are the same

--- src/main/kotlin/com/cynergisuite/middleware/audit/infrastructure/AuditRepository.kt
+++ src/main/kotlin/com/cynergisuite/middleware/audit/infrastructure/AuditRepository.kt
@@ -441,6 +441,7 @@ class AuditRepository @Inject constructor(
                SELECT COUNT (id)
                FROM fastinfo_prod_import.inventory_vw i
                WHERE i.primary_location = :store_number
+                     AND i.location = :store_number
                      AND i.status in ('N', 'R')
                      AND i.dataset = :dataset
             ),
