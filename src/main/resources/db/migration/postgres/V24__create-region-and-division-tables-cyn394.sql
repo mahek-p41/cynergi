89417343cc7a2c2d51c124fba62196a5f5c82063
beckyk <beckyk@hightouchinc.com>
2020-02-04 09:20:14 -0600

    Cyn394 create division and region revised syntax.

--- src/main/resources/db/migration/postgres/V24__create-region-and-division-tables-cyn394.sql
+++ src/main/resources/db/migration/postgres/V24__create-region-and-division-tables-cyn394.sql
@@ -4,10 +4,15 @@ CREATE TABLE division
     uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
     time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
     time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    company_id         INTEGER REFERENCES company(id),
+    company_id         BIGINT REFERENCES company(id) NOT NULL,
     number             INTEGER DEFAULT 1 NOT NULL,
     name               varchar(50),
-    description        varchar(50)
+    description        varchar(50),
+    CONSTRAINT uq_division UNIQUE
+    ( id,
+      number,
+      company_id
+    )
  );
 CREATE TRIGGER update_division_trg
    BEFORE UPDATE
@@ -21,6 +26,7 @@ CREATE TABLE region
     uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
     time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
     time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
+    company_id         BIGINT REFERENCES company(id)                        NOT NULL,
     number             INTEGER DEFAULT 1 NOT NULL,
     name               varchar(50),
     description        varchar(50)
@@ -33,68 +39,48 @@ EXECUTE PROCEDURE last_updated_column_fn();
 
 CREATE TABLE division_to_region
  (
-    region_number      INTEGER NOT NULL,
-    division_number    INTEGER NOT NULL,
+    division_id BIGINT REFERENCES division(id) NOT NULL,
+    company_id BIGINT REFERENCES company(id) NOT NULL,
+    region_id BIGINT REFERENCES region(id) NOT NULL,
     CONSTRAINT uq_division_to_region UNIQUE
-    ( region_number,
-      division_number
+    ( division_id,
+      company_id,
+      region_id
     )
  );
 
-CREATE TRIGGER update_division_to_region_trg
-   BEFORE UPDATE
-   ON division_to_region
-   FOR EACH ROW
-EXECUTE PROCEDURE last_updated_column_fn();
 
 CREATE TABLE region_to_store
  (
-    region_number         INTEGER NOT NULL,
-    store_number          INTEGER NOT NULL
+    region_id BIGINT REFERENCES region(id) NOT NULL,
+    company_id BIGINT REFERENCES company(id) NOT NULL,
+    store_number INTEGER  CHECK( store_number > 0 ) NOT NULL,
+    CONSTRAINT uq_region_to_store UNIQUE
+    ( region_id,
+      company_id,
+      store_number
+    )
  );
-
-
-CREATE TRIGGER update_division_to_region_trg
-   BEFORE UPDATE
-   ON region_to_store
-   FOR EACH ROW
-EXECUTE PROCEDURE last_updated_column_fn();
 
 
 CREATE TABLE division_manager
  (
-    id                 BIGSERIAL   NOT NULL PRIMARY KEY,
-    uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
-    time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    division_number    INTEGER,
-    emp_number         INTEGER,
-    name               varchar(50),
-    active             BOOLEAN DEFAULT 'No',
-    inactive_date      Date
+    division_id  BIGINT REFERENCES division(id) NOT NULL,
+    employee_number INTEGER CHECK( employee_number > 0 ) NOT NULL,
+    CONSTRAINT uq_division_manager UNIQUE
+    ( division_id,
+      employee_number
+    )
  );
-CREATE TRIGGER update_division_manager_trg
-   BEFORE UPDATE
-   ON division_manager
-   FOR EACH ROW
-EXECUTE PROCEDURE last_updated_column_fn();
 
 CREATE TABLE region_manager
  (
-    id                 BIGSERIAL   NOT NULL PRIMARY KEY,
-    uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
-    time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    region_number      INTEGER,
-    emp_number         INTEGER,
-    name               varchar(50),
-    active             BOOLEAN DEFAULT 'No',
-    inactive_date      Date
+   region_id BIGINT REFERENCES region(id) NOT NULL,
+   employee_number INTEGER CHECK( employee_number > 0 ) NOT NULL,
+   CONSTRAINT uq_region_manager UNIQUE
+    ( region_id,
+      employee_number
+    )
  );
-CREATE TRIGGER update_region_manager_trg
-   BEFORE UPDATE
-   ON region_manager
-   FOR EACH ROW
-EXECUTE PROCEDURE last_updated_column_fn();
 
 
