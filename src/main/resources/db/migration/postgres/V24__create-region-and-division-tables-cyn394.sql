1f315fbde00cb2368a5d91d6e1cedb7cf5ca03b3
beckyk <beckyk@hightouchinc.com>
2020-02-05 10:23:50 -0600

    Create statements division and region.

--- src/main/resources/db/migration/postgres/V24__create-region-and-division-tables-cyn394.sql
+++ src/main/resources/db/migration/postgres/V24__create-region-and-division-tables-cyn394.sql
@@ -6,12 +6,14 @@ CREATE TABLE division
     time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
     company_id         BIGINT REFERENCES company(id) NOT NULL,
     number             INTEGER DEFAULT 1 NOT NULL,
-    name               varchar(50),
+    name               varchar(50) DEFAULT 'Division' NOT NULL,
+    employee_number    INTEGER CHECK( employee_number > 0 ) NOT NULL,
     description        varchar(50),
     CONSTRAINT uq_division UNIQUE
     ( id,
+      company_id,
       number,
-      company_id
+      employee_number
     )
  );
 CREATE TRIGGER update_division_trg
@@ -26,28 +28,23 @@ CREATE TABLE region
     uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
     time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
     time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
-    company_id         BIGINT REFERENCES company(id)                        NOT NULL,
+    division_id        BIGINT REFERENCES division(id)                       NOT NULL,
     number             INTEGER DEFAULT 1 NOT NULL,
-    name               varchar(50),
-    description        varchar(50)
+    name               varchar(50) DEFAULT 'Region' NOT NULL,
+    employee_number    INTEGER CHECK( employee_number > 0 ) NOT NULL,
+    description        varchar(50),
+    CONSTRAINT uq_region UNIQUE
+    ( id,
+      division_id,
+      number,
+      employee_number
+    )
  );
 CREATE TRIGGER update_region_trg
    BEFORE UPDATE
    ON region
    FOR EACH ROW
 EXECUTE PROCEDURE last_updated_column_fn();
-
-CREATE TABLE division_to_region
- (
-    division_id BIGINT REFERENCES division(id) NOT NULL,
-    company_id BIGINT REFERENCES company(id) NOT NULL,
-    region_id BIGINT REFERENCES region(id) NOT NULL,
-    CONSTRAINT uq_division_to_region UNIQUE
-    ( division_id,
-      company_id,
-      region_id
-    )
- );
 
 
 CREATE TABLE region_to_store
@@ -63,24 +60,6 @@ CREATE TABLE region_to_store
  );
 
 
-CREATE TABLE division_manager
- (
-    division_id  BIGINT REFERENCES division(id) NOT NULL,
-    employee_number INTEGER CHECK( employee_number > 0 ) NOT NULL,
-    CONSTRAINT uq_division_manager UNIQUE
-    ( division_id,
-      employee_number
-    )
- );
 
-CREATE TABLE region_manager
- (
-   region_id BIGINT REFERENCES region(id) NOT NULL,
-   employee_number INTEGER CHECK( employee_number > 0 ) NOT NULL,
-   CONSTRAINT uq_region_manager UNIQUE
-    ( region_id,
-      employee_number
-    )
- );
 
 
