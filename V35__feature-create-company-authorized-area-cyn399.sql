2190834df93cf0ac9a3a7059ab2a0d0d49d7ef6e
garym <garym@hightouchinc.com>
2020-03-10 14:57:04 -0500

    Merged in develop
    Merged 3 migrations into single migration

--- V35__feature-create-company-authorized-area-cyn399.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE company_to_area_type (
-	 company_id BIGINT REFERENCES company(id)                                    NOT NULL,
-	 area_type_id  BIGINT REFERENCES area_type_domain(id)                        NOT NULL,
-	 UNIQUE(company_id,area_type_id)
- );
-
-COMMENT ON TABLE company_to_area_type IS 'contains the id associated with the individual modules/systems that the company has agreed to implement. i.e. PO, AP, GL';
