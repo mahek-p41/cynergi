e5cc307cdb82cee0b27d475e082fc7d43dded463
garym <garym@hightouchinc.com>
2020-03-23 17:40:11 -0500

    Removed useless postgres-started script
    fixed sed find replace

--- support/deployment/cynmid/build-cynmid.sh
+++ support/deployment/cynmid/build-cynmid.sh
@@ -21,9 +21,7 @@ mkdir -p /opt/cyn/v01/cynmid/data/
 cp /home/jenkins/cynergi-middleware/support/deployment/cyndsets-parse.sh /opt/cyn/v01/cynmid/data/cyndsets-parse.sh
 chmod u+x /opt/cyn/v01/cynmid/data/cyndsets-parse.sh
 cp /home/jenkins/cynergi-middleware/support/deployment/cynergi-middleware.httpd.conf /opt/cyn/v01/cynmid/cynergi-middleware.httpd.conf
-sed "s/@@JAVA_VER_BUILD@@/${VER_BUILD}/g" /home/jenkins/cynergi-middleware/support/deployment/cynergi-middleware.conf > /opt/cyn/v01/cynmid/cynergi-middleware.conf
-sed "s/@@MICRONAUT_ENV@@/${MICRONAUT_ENV}/g" /opt/cyn/v01/cynmid/cynergi-middleware.conf > /opt/cyn/v01/cynmid/cynergi-middleware.conf
-cp /home/jenkins/cynergi-middleware/support/deployment/postgres-started.service /opt/cyn/v01/cynmid/data/
+sed "s/@@JAVA_VER_BUILD@@/${VER_BUILD}/g; s/@@MICRONAUT_ENV@@/${MICRONAUT_ENV}/g" /home/jenkins/cynergi-middleware/support/deployment/cynergi-middleware.conf > /opt/cyn/v01/cynmid/cynergi-middleware.conf
 cp /home/jenkins/cynergi-middleware/support/deployment/setup-database.sql /opt/cyn/v01/cynmid/data/
 cp /home/jenkins/cynergi-middleware/build/libs/cynergi-middleware*-all.jar /opt/cyn/v01/cynmid/cynergi-middleware.jar
 mkdir -p /opt/cyn/v01/cynmid/java/openj9/${VER_BUILD}/jitcache
