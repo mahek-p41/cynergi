fa0c67d1dce410f70013b88a64f108b72a678bb6
beckyk <beckyk@hightouchinc.com>
2020-03-30 11:42:51 -0500

    Changes to create account payable control migration.

--- src/main/resources/db/migration/postgres/V39__feature-create-ap-ctl-cyn362.sql
+++ src/main/resources/db/migration/postgres/V39__feature-create-ap-ctl-cyn362.sql
@@ -1,4 +1,34 @@
-CREATE TABLE accounts_payable_control (
+CREATE TABLE print_currency_indicator_type_domain (
+	 id integer                                                                   NOT NULL PRIMARY KEY,
+    value varchar(10) CHECK ( char_length(trim(value)) > 0)                      NOT NULL,
+	 description varchar(100) CHECK ( char_length(trim(description)) > 1)         NOT NULL,
+	 localization_code varchar(100) CHECK( char_length(trim(localization_code)) > 1) NOT NULL,
+	 UNIQUE(value)
+ );
+
+INSERT INTO print_currency_indicator_type_domain(id,value,description,localization_code)
+Values
+(1,'B', 'Bank', 'bank'),
+(2,'N', 'No', 'no'),
+(3,'V', 'Vendor', 'vendor');
+
+
+CREATE TABLE purchase_order_number_required_indicator_type_domain (
+	 id integer                                                                   NOT NULL PRIMARY KEY,
+    value varchar(10) CHECK ( char_length(trim(value)) > 0)                      NOT NULL,
+	 description varchar(100) CHECK ( char_length(trim(description)) > 1)         NOT NULL,
+	 localization_code varchar(100) CHECK( char_length(trim(localization_code)) > 1) NOT NULL,
+	 UNIQUE(value)
+ );
+
+INSERT INTO purchase_order_number_required_indicator_type_domain(id,value,description,localization_code)
+Values
+(1,'M', 'Sometimes Validate', 'sometimes.validate'),
+(2,'N', 'Never Validate', 'never.validate'),
+(3,'V', 'Validate', 'validate');
+
+
+CREATE TABLE account_payable_control(
     id                 BIGSERIAL                                            NOT NULL PRIMARY KEY,
     uu_row_id          UUID        DEFAULT uuid_generate_v1()               NOT NULL,
     time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                NOT NULL,
@@ -14,14 +44,44 @@ CREATE TABLE accounts_payable_control (
     general_ledger_corporate_sales_id  BIGINT REFERENCES account(id)  NOT NULL,
     general_ledger_corporate_purchase_id BIGINT REFERENCES account(id)  NOT NULL,
     general_ledger_inventory_account_id BIGINT REFERENCES account(id)  NOT NULL,
-    print_currency_indicator            varchar(1) DEFAULT 'N'              NOT NULL,
+    print_currency_indicator_type_id BIGINT REFERENCES print_currency_indicator_type_domain(id)  NOT NULL,
     lock_inventory_indicator            BOOLEAN DEFAULT FALSE               NOT NULL,
-    purchase_order_number_required_indicator varchar(1)  DEFAULT 'V'        NOT NULL
+    purchase_order_number_required_indicator_type_id BIGINT REFERENCES purchase_order_number_required_indicator_type_domain (id)    NOT NULL,
+    UNIQUE(compnany_id)
 );
 
-
-CREATE TRIGGER accounts_payable_control_trg
+CREATE TRIGGER account_payable_control_trg
    BEFORE UPDATE
-   ON accounts_payable_control
+   ON account_payable_control
    FOR EACH ROW
 EXECUTE PROCEDURE last_updated_column_fn();
+
+CREATE INDEX idx_account_payable_control_company_id
+    ON account_payable_control (company_id);
+
+CREATE INDEX idx_general_ledger_inventory_clearing_account_id
+   ON account_payable_control (general_ledger_inventory_clearing_account_id);
+
+CREATE INDEX idx_general_ledger_store_accounts_payable_id
+    ON account_payable_control (general_ledger_store_accounts_payable_id);
+
+CREATE INDEX idx_general_ledger_corporate_accounts_receivable_id
+   ON account_payable_control (general_ledger_corporate_accounts_receivable_id);
+
+CREATE INDEX idx_general_ledger_corporate_sales_id
+  ON account_payable_control (general_ledger_corporate_sales_id);
+
+CREATE INDEX idx_general_ledger_corporate_purchase_id
+  ON account_payable_control (general_ledger_corporate_purchase_id);
+
+  CREATE INDEX idx_general_ledger_inventory_account_id
+    ON account_payable_control (general_ledger_inventory_account_id);
+
+CREATE INDEX idx_print_currency_indicator_type_id
+   ON account_payable_control (print_currency_indicator_type_id);
+
+CREATE INDEX idx_purchase_order_number_required_indicator_type_id
+  ON account_payable_control (purchase_order_number_required_indicator_type_id);
+
+
+
