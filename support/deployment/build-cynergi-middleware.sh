adb44eb62065ffb4bda28b4a45fc43b12f3db45d
garym <garym@hightouchinc.com>
2020-03-31 16:28:37 -0500

    Added caching of gradle libraries

--- support/deployment/build-cynergi-middleware.sh
+++ support/deployment/build-cynergi-middleware.sh
@@ -3,6 +3,7 @@
 export USER_ID=$(id -u)
 export GROUP_ID=$(cut -d: -f3 < <(getent group "${USER}"))
 
+mkdir gradleCache
 docker rmi deployment_cynmid
 docker-compose rm -f
 docker-compose build --build-arg USER_ID="${USER_ID}" --build-arg GROUP_ID="${GROUP_ID}"  cynmid
