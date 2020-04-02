91fde760cce098685b0283458420a6646c1ee1d2
beckyk <beckyk@hightouchinc.com>
2020-04-02 09:25:26 -0500

    Create account payable control record and associated domain tables.

--- src/main/resources/db/migration/postgres/V39__feature-create-ap-ctl-cyn362.sql
+++ src/main/resources/db/migration/postgres/V39__feature-create-ap-ctl-cyn362.sql
@@ -37,7 +37,7 @@ CREATE TABLE account_payable_control(
     pay_after_discount_date             BOOLEAN DEFAULT FALSE               NOT NULL,
     reset_expense                       BOOLEAN DEFAULT FALSE               NOT NULL,
     use_rebates_indicator               BOOLEAN DEFAULT FALSE               NOT NULL,
-    general_ledger_inventory_clearing_account_id BIGINT REFERENCES account(id) NOT HULL,
+    general_ledger_inventory_clearing_account_id BIGINT REFERENCES account(id) NOT NULL,
     trade_company_indicator                    BOOLEAN DEFAULT FALSE        NOT NULL,
     general_ledger_store_accounts_payable_id BIGINT REFERENCES account(id)  NOT NULL,
     general_ledger_corporate_accounts_receivable_id  BIGINT REFERENCES account(id)  NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE account_payable_control(
     print_currency_indicator_type_id BIGINT REFERENCES print_currency_indicator_type_domain(id)  NOT NULL,
     lock_inventory_indicator            BOOLEAN DEFAULT FALSE               NOT NULL,
     purchase_order_number_required_indicator_type_id BIGINT REFERENCES purchase_order_number_required_indicator_type_domain (id)    NOT NULL,
-    UNIQUE(compnany_id)
+    UNIQUE(company_id)
 );
 
 CREATE TRIGGER account_payable_control_trg
@@ -56,31 +56,31 @@ CREATE TRIGGER account_payable_control_trg
    FOR EACH ROW
 EXECUTE PROCEDURE last_updated_column_fn();
 
-CREATE INDEX idx_account_payable_control_company_id
+CREATE INDEX idx_account_payable_ctl_company_id
     ON account_payable_control (company_id);
 
-CREATE INDEX idx_general_ledger_inventory_clearing_account_id
+CREATE INDEX idx_acount_payable_ctl_gl_inventory_clearing_account_id
    ON account_payable_control (general_ledger_inventory_clearing_account_id);
 
-CREATE INDEX idx_general_ledger_store_accounts_payable_id
+CREATE INDEX idx_account_payable_ctl_gl_store_accounts_payable_id
     ON account_payable_control (general_ledger_store_accounts_payable_id);
 
-CREATE INDEX idx_general_ledger_corporate_accounts_receivable_id
+CREATE INDEX idx_account_payable_ctl_gl_corporate_accounts_receivable_id
    ON account_payable_control (general_ledger_corporate_accounts_receivable_id);
 
-CREATE INDEX idx_general_ledger_corporate_sales_id
+CREATE INDEX idx_account_payable_ctl_gl_corporate_sales_id
   ON account_payable_control (general_ledger_corporate_sales_id);
 
-CREATE INDEX idx_general_ledger_corporate_purchase_id
+CREATE INDEX idx_account_payable_ctl_gl_corporate_purchase_id
   ON account_payable_control (general_ledger_corporate_purchase_id);
 
-  CREATE INDEX idx_general_ledger_inventory_account_id
+  CREATE INDEX idx_account_payable_ctl_gl_inventory_account_id
     ON account_payable_control (general_ledger_inventory_account_id);
 
-CREATE INDEX idx_print_currency_indicator_type_id
+CREATE INDEX idx_account_payable_ctl_print_currency_indicator_type_id
    ON account_payable_control (print_currency_indicator_type_id);
 
-CREATE INDEX idx_purchase_order_number_required_indicator_type_id
+CREATE INDEX idx_account_payable_ctl_po_nbr_required_indicator_type_id
   ON account_payable_control (purchase_order_number_required_indicator_type_id);
 
 
