d4a80b0895d8670fadf70d3664265c460e0f3936
beckyk <beckyk@hightouchinc.com>
2020-04-09 11:51:03 -0500

    Modifications to vendor_payment_term and address migrations.

--- src/main/resources/db/migration/postgres/V36__feature-address-vendor-payment-term-cyn395.sql
+++ src/main/resources/db/migration/postgres/V36__feature-address-vendor-payment-term-cyn395.sql
@@ -16,6 +16,8 @@ CREATE TABLE address
     longitude numeric (14,11),
     country varchar(50)CHECK (char_length(trim(city)) > 1)                      NOT NULL,
     county varchar(50),
+    phone  varchar(18),
+    fax  varchar(18),
     UNIQUE (company_id, number)
 );
 
@@ -31,6 +33,8 @@ CREATE TRIGGER update_address_trg
    FOR EACH ROW
 EXECUTE PROCEDURE last_updated_column_fn();
 
+
+
 CREATE TABLE vendor_payment_term (
     id                 BIGSERIAL                                                NOT NULL PRIMARY KEY,
     uu_row_id          UUID        DEFAULT uuid_generate_v1()                   NOT NULL,
@@ -40,31 +44,43 @@ CREATE TABLE vendor_payment_term (
     description varchar(30) CHECK ( char_length(trim(description)) > 1)         NOT NULL,
     number integer CHECK (number > 0)                                           NOT NULL,
     number_of_payments integer DEFAULT 1                                        NOT NULL,
-    due_month_1  integer,
-    due_month_2  integer,
-    due_month_3  integer,
-    due_month_4  integer,
-    due_month_5  integer,
-    due_month_6  integer,
-    due_days_1  integer,
-    due_days_2  integer,
-    due_days_3  integer,
-    due_days_4  integer,
-    due_days_5  integer,
-    due_days_6  integer,
-    due_percent_1 numeric(8,4),
-    due_percent_2 numeric(8,4),
-    due_percent_3 numeric(8,4),
-    due_percent_4 numeric(8,4),
-    due_percent_5 numeric(8,4),
-    due_percent_6 numeric(8,4),
     discount_month  integer,
     discount_days  integer,
     discount_percent numeric(6,2),
     UNIQUE (company_id, number)
 );
+
+CREATE INDEX idx_vendor_payment_term_company_id
+ON vendor_payment_term(company_id);
+
 CREATE TRIGGER update_vendor_payment_term_trg
    BEFORE UPDATE
    ON vendor_payment_term
    FOR EACH ROW
 EXECUTE PROCEDURE last_updated_column_fn();
+
+
+CREATE TABLE vendor_payment_term_schedule (
+    id                 BIGSERIAL                                                NOT NULL PRIMARY KEY,
+    uu_row_id          UUID        DEFAULT uuid_generate_v1()                   NOT NULL,
+    time_created       TIMESTAMPTZ DEFAULT clock_timestamp()                    NOT NULL,
+    time_updated       TIMESTAMPTZ DEFAULT clock_timestamp()                    NOT NULL,
+    payment_term_id BIGINT REFERENCES vendor_payment_term (id)                  NOT NULL,
+    description varchar(30) CHECK ( char_length(trim(description)) > 1)         NOT NULL,
+    due_month  integer CHECK ( due_month > 0 )                                  NOT NULL,
+    due_days  integer CHECK ( due_days > 0 )                                    NOT NULL,
+    due_percent numeric(8,4) CHECK ( due_percent > 0 )                          NOT NULL,
+    schedule_order_number integer  CHECK ( schedule_order_number > 0 )          NOT NULL
+ );
+
+CREATE INDEX idx_vendor_payment_schedule_payment_term_id
+ON vendor_payment_term_schedule(payment_term_id);
+
+
+CREATE TRIGGER update_vendor_payment_term_schedule_trg
+   BEFORE UPDATE
+   ON vendor_payment_term_schedule
+   FOR EACH ROW
+EXECUTE PROCEDURE last_updated_column_fn();
+
+
