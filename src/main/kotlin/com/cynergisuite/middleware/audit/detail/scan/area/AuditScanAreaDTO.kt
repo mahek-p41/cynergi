bfdfd15629ede4baf235945799eb4001c8162e40
vun <vun@hightouchinc.com>
2020-10-14 14:22:48 -0500

    Add value & description into ScanAreaDTO
    This is a workaround to keep the Inventory App unchanged so we can roll out for some clients first.

--- src/main/kotlin/com/cynergisuite/middleware/audit/detail/scan/area/AuditScanAreaDTO.kt
+++ src/main/kotlin/com/cynergisuite/middleware/audit/detail/scan/area/AuditScanAreaDTO.kt
@@ -5,8 +5,10 @@ import com.cynergisuite.middleware.store.StoreValueObject
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import io.swagger.v3.oas.annotations.media.Schema
+import javax.annotation.Nullable
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
+import javax.validation.constraints.Size
 
 @JsonInclude(NON_NULL)
 @Schema(name = "AuditScanAreaEntity", title = "Area where an item was scanned", description = "Possible location within a store where an item was scanned as part of an audit")
@@ -20,6 +22,16 @@ data class AuditScanAreaDTO(
    @field:Schema(description = "Scan area name")
    var name: String? = null,
 
+   @field:Nullable
+   @field:Size(min = 3, max = 15)
+   @field:Schema(description = "This is a database driven with the original values being SHOWROOM, STOREROOM and WAREHOUSE")
+   var value: String? = null,
+
+   @field:Nullable
+   @field:Size(min = 3, max = 50)
+   @field:Schema(description = "A localized description suitable for showing the user")
+   var description: String? = null,
+
    @field:NotNull
    @field:Schema(description = "A store that scan area belong to")
    var store: StoreValueObject? = null
@@ -31,6 +43,8 @@ data class AuditScanAreaDTO(
       this(
          id = entity.myId(),
          name = entity.name,
+         value = entity.name,
+         description = entity.name,
          store = entity.store?.let { StoreValueObject(it) }
       )
 }
