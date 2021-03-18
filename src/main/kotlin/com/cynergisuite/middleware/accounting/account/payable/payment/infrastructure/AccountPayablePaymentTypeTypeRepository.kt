c91c610ccffb85599b1f6397a9d7652674d262ad
sabrinas <sabrinas@hightouchinc.com>
2021-03-18 11:01:37 -0500

    Create ap payment type type backend

---
+++ src/main/kotlin/com/cynergisuite/middleware/accounting/account/payable/payment/infrastructure/AccountPayablePaymentTypeTypeRepository.kt
@@ -0,0 +1,62 @@
+package com.cynergisuite.middleware.accounting.account.payable.payment.infrastructure
+
+import com.cynergisuite.extensions.findFirstOrNull
+import com.cynergisuite.middleware.accounting.account.payable.payment.AccountPayablePaymentTypeType
+import org.apache.commons.lang3.StringUtils.EMPTY
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import java.sql.ResultSet
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AccountPayablePaymentTypeTypeRepository @Inject constructor(
+   private val jdbc: NamedParameterJdbcTemplate
+) {
+   private val logger: Logger = LoggerFactory.getLogger(AccountPayablePaymentTypeTypeRepository::class.java)
+
+   fun exists(value: String): Boolean {
+      val exists = jdbc.queryForObject("SELECT EXISTS (SELECT id FROM account_payable_payment_type_type_domain WHERE UPPER(value) = :value", mapOf("value" to value.toUpperCase()), Boolean::class.java)!!
+
+      logger.trace("Checking if AccountPayablePaymentTypeType: {} exists resulting in {}", value, exists)
+
+      return exists
+   }
+
+   fun findOne(id: Long): AccountPayablePaymentTypeType? {
+      val params = mutableMapOf<String, Any?>("id" to id)
+      val query = "SELECT * FROM account_payable_payment_type_type_domain WHERE id = :id"
+      logger.trace("Searching for AccountPayablePaymentTypeTypeDomain {}: \nQuery {}", params, query)
+
+      val found = jdbc.findFirstOrNull(query, params, RowMapper { rs, _ -> mapRow(rs) })
+
+      logger.trace("Searching for AccountPayablePaymentTypeTypeDomain {}: \nQuery {} \nResulted in {}", params, query, found)
+
+      return found
+   }
+
+   fun findOne(value: String): AccountPayablePaymentTypeType? {
+      val params = mutableMapOf<String, Any?>("value" to value.toUpperCase())
+      val query = "SELECT * FROM account_payable_payment_type_type_domain WHERE UPPER(value) = :value"
+      logger.trace("Searching for AccountPayablePaymentTypeTypeDomain {}: \nQuery {}", params, query)
+
+      val found = jdbc.findFirstOrNull(query, params, RowMapper { rs, _ -> mapRow(rs) })
+
+      logger.trace("Searching for AccountPayablePaymentTypeTypeDomain {}: \nQuery {} \nResulted in {}", params, query, found)
+
+      return found
+   }
+
+   fun findAll(): List<AccountPayablePaymentTypeType> =
+      jdbc.query("SELECT * FROM account_payable_payment_type_type_domain ORDER BY id") { rs, _ -> mapRow(rs) }
+
+   fun mapRow(rs: ResultSet, columnPrefix: String = EMPTY): AccountPayablePaymentTypeType =
+      AccountPayablePaymentTypeType(
+         id = rs.getLong("${columnPrefix}id"),
+         value = rs.getString("${columnPrefix}value"),
+         description = rs.getString("${columnPrefix}description"),
+         localizationCode = rs.getString("${columnPrefix}localization_code")
+      )
+}
