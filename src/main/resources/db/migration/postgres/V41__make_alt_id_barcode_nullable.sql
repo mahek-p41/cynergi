798a97a66fe84f555b5b55b1005644c86253ef2a
vun <vun@hightouchinc.com>
2021-06-07 10:46:15 -0500

    Fix v41 flyway script handling existing null lookup_key

--- src/main/resources/db/migration/postgres/V41__make_alt_id_barcode_nullable.sql
+++ src/main/resources/db/migration/postgres/V41__make_alt_id_barcode_nullable.sql
@@ -1,8 +1,18 @@
+UPDATE audit_exception
+SET lookup_key = barcode
+WHERE lookup_key is NULL
+;
+
 ALTER TABLE audit_exception
    DROP CONSTRAINT IF EXISTS audit_exception_alt_id_check,
    DROP CONSTRAINT IF EXISTS audit_exception_bar_code_check,
    ALTER COLUMN barcode DROP NOT NULL,
    ALTER COLUMN lookup_key SET NOT NULL
+;
+
+UPDATE audit_detail
+SET lookup_key = barcode
+WHERE lookup_key is NULL
 ;
 
 ALTER TABLE audit_detail
