ed66ea32ed7d6b31c57e3dc0320efaa2cf2c34f0
garym <garym@hightouchinc.com>
2021-08-23 10:10:12 -0500

    Moved the bulk of the V72 migration SQL script to a repeatable script.

--- src/main/resources/db/migration/postgres/repeatable/R__app-setup.sql
+++ src/main/resources/db/migration/postgres/repeatable/R__app-setup.sql
@@ -1,18 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
--- create wrapper function for hashing passwords
-CREATE OR REPLACE FUNCTION hash_passcode(TEXT)
-    RETURNS TEXT AS
-$$
-BEGIN
-    IF $1 IS NOT NULL AND length($1) > 2 THEN
-        RETURN crypt($1, gen_salt('bf', 10));
-    ELSE
-        RAISE EXCEPTION 'Pass code provided does not meet length requirement of 3';
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
 -- setup a simple view for getting employees that can be authenticated against
 DO $$
 DECLARE
