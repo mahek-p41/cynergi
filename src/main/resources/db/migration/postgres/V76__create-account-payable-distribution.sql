4e2ea90d07e23b5cbbf14613ab61c21d224370d7
Murphy Newell <murphyn@hightouchinc.com>
2021-11-02 15:47:22 -0400

    adding new migration

---
+++ src/main/resources/db/migration/postgres/V76__create-account-payable-distribution.sql
@@ -0,0 +1,16 @@
+CREATE TABLE account_payable_distribution
+(
+   id                                  UUID        DEFAULT uuid_generate_v1()          NOT NULL PRIMARY KEY,
+   time_created                        TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()           NOT NULL,
+   time_updated                        TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()           NOT NULL,
+   payment_id                          UUID REFERENCES account_payable_payment(id)     NOT NULL,
+   distribution_account                UUID REFERENCES account(id)                     NOT NULL,
+   distribution_profit_center_sfk      INTEGER,
+   distribution_amount                 NUMERIC(11,2)
+);
+
+CREATE TRIGGER account_payable_distribution_trg
+   BEFORE UPDATE
+   ON account_payable_distribution
+   FOR EACH ROW
+EXECUTE PROCEDURE update_user_table_fn();
