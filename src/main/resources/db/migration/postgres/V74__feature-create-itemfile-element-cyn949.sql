5f724c35241107ce42f6a2227bad27543c140061
murphy <murphyn@hightouchinc.com>
2021-11-23 10:00:42 -0500

    updated as per Vu

--- src/main/resources/db/migration/postgres/V74__feature-create-itemfile-element-cyn949.sql
+++ src/main/resources/db/migration/postgres/V74__feature-create-itemfile-element-cyn949.sql
@@ -5,14 +5,14 @@ CREATE TABLE itemfile_element
     time_updated                 TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()                          NOT NULL,
     company_id                   UUID REFERENCES company (id)                                   NOT NULL,
     itemfile_id                  UUID                                                           NOT NULL,
-    itemfile_sfk                 BIGINT                                                         NOT NULL,
-    number                       BIGINT                                                         NOT NULL,
+    itemfile_sfk                 INTEGER CHECK( number > 0 )                                    NOT NULL,
+    number                       INTEGER CHECK( number > 0 )                                    NOT NULL,
     vendor_model_number          VARCHAR(25)                                                    NOT NULL,
     element_desc                 VARCHAR(100) CHECK ( char_length(trim(element_desc)) > 1)      NOT NULL,
-    po_qty_per_order             INTEGER                                                        NOT NULL,
+    po_qty_per_order             INTEGER CHECK( number > 0 )                                    NOT NULL,
     cost_per_po_qty              NUMERIC(11, 3)                                                 NOT NULL,
-    color_id_sfk                 INTEGER,
-    fabric_color_id_sfk          INTEGER
+    color_id_sfk                 INTEGER CHECK( number > 0 )                                    NOT NULL,
+    fabric_color_id_sfk          INTEGER CHECK( number > 0 )                                    NOT NULL
 );
 CREATE TRIGGER itemfile_element_trg
     BEFORE UPDATE
--- src/main/resources/db/migration/postgres/V75__feature-create-po-detail-element-cyn421.sql
+++ src/main/resources/db/migration/postgres/V75__feature-create-po-detail-element-cyn421.sql
@@ -5,13 +5,13 @@ CREATE TABLE purchase_order_detail_element
     time_updated                 TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()                          NOT NULL,
     company_id                   UUID REFERENCES company (id)                                   NOT NULL,
     purchase_order_detail_id     UUID REFERENCES purchase_order_detail (id)                     NOT NULL,
-    number                       BIGINT                                                         NOT NULL,
+    number                       INTEGER CHECK( number > 0 )                                    NOT NULL,
     vendor_model_number_id       VARCHAR(25)                                                    NOT NULL,
     element_desc                 VARCHAR(100) CHECK ( char_length(trim(element_desc)) > 1)      NOT NULL,
-    po_qty_per_order             INTEGER                                                        NOT NULL,
+    po_qty_per_order             INTEGER CHECK( number > 0 )                                    NOT NULL,
     cost_per_po_qty              NUMERIC(11, 3)                                                 NOT NULL,
-    color_id_sfk                 INTEGER,
-    fabric_color_id_sfk          INTEGER
+    color_id_sfk                 INTEGER CHECK( number > 0 )                                    NOT NULL,
+    fabric_color_id_sfk          INTEGER CHECK( number > 0 )                                    NOT NULL
 );
 CREATE TRIGGER purchase_order_detail_element_trg
     BEFORE UPDATE
