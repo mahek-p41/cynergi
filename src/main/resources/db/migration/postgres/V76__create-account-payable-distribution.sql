1d0e19052326beb60f450d5da3d0168ed144a905
murphy <murphyn@hightouchinc.com>
2021-11-23 10:10:41 -0500

    updated as per Vu

--- src/main/resources/db/migration/postgres/V76__create-account-payable-distribution.sql
+++ src/main/resources/db/migration/postgres/V76__create-account-payable-distribution.sql
@@ -1,12 +1,12 @@
-CREATE TABLE account_payable_distribution
+CREATE TABLE account_payable_payment_distribution
 (
    id                                  UUID        DEFAULT uuid_generate_v1()          NOT NULL PRIMARY KEY,
    time_created                        TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()           NOT NULL,
    time_updated                        TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP()           NOT NULL,
    payment_id                          UUID REFERENCES account_payable_payment(id)     NOT NULL,
    distribution_account                UUID REFERENCES account(id)                     NOT NULL,
-   distribution_profit_center_sfk      INTEGER,
-   distribution_amount                 NUMERIC(11,2)
+   distribution_profit_center_sfk      INTEGER                                         NOT NULL,
+   distribution_amount                 NUMERIC(11,2)                                   NOT NULL
 );
 
 CREATE TRIGGER account_payable_distribution_trg
