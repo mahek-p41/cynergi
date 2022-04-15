be59d5b887e505c09429425170fa5bbac323059e
murphy <murphyn@hightouchinc.com>
2022-04-15 13:36:11 -0400

    changed migration script to just drop and create new index

--- src/main/resources/db/migration/postgres/V86__alter-gl-source-codes-table.sql
+++ src/main/resources/db/migration/postgres/V86__alter-gl-source-codes-table.sql
@@ -1,6 +1,5 @@
-CREATE UNIQUE INDEX newidx ON general_ledger_source_codes USING btree (company_id, LOWER(value), deleted)
-WHERE deleted = false;
-
 DROP INDEX general_ledger_source_codes_unique_idx;
 
-ALTER INDEX newidx RENAME to general_ledger_source_codes_unique_idx;
+CREATE UNIQUE INDEX general_ledger_source_codes_unique_idx ON general_ledger_source_codes USING btree (company_id, LOWER(value), deleted)
+WHERE deleted = false;
+
