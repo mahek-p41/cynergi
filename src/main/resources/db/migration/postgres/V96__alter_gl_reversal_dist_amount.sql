dcbca0cd8693b7781f00b6d92ab079004d594af1
vun <vun@hightouchinc.com>
2022-07-29 14:02:39 -0500

    Revert "Altered gl recurring distribution amount field"
    This reverts commit 89a352cd8f9ee619972f14c796e8e73f6824dee5.

--- src/main/resources/db/migration/postgres/V96__alter_gl_reversal_dist_amount.sql
+++ src/main/resources/db/migration/postgres/V96__alter_gl_reversal_dist_amount.sql
@@ -1,5 +1,2 @@
 ALTER TABLE general_ledger_reversal_distribution
 ALTER COLUMN general_ledger_reversal_distribution_amount TYPE NUMERIC(13, 2);
-
-ALTER TABLE general_ledger_recurring_distribution
-ALTER COLUMN general_ledger_distribution_amount TYPE NUMERIC(13, 2);
