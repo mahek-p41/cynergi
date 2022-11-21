566fde298b020eb5d4774de840c0d51591a07ed6
murphy <murphyn@hightouchinc.com>
2022-11-21 16:11:43 -0500

    cash flow report simple done

---
+++ src/main/kotlin/com/cynergisuite/middleware/accounting/account/payable/cashflow/CashFlowVendorDTO.kt
@@ -0,0 +1,43 @@
+package com.cynergisuite.middleware.accounting.account.payable.cashflow
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+import javax.validation.constraints.Size
+
+data class CashFlowVendorDTO (
+
+@field:NotNull
+@field:Schema(description = "Vendor company id")
+var vendorCompanyId: UUID? = null,
+
+@field:Positive
+@field:Schema(description = "Vendor number")
+var vendorNumber: Int? = null,
+
+@field:NotNull
+@field:NotBlank
+@field:Size(min = 1, max = 30)
+@field:Schema(description = "Vendor name")
+var vendorName: String? = null,
+
+@field:NotNull
+@field:Schema(description = "Invoices")
+var invoices: MutableSet<CashFlowReportInvoiceDetailEntity>? = LinkedHashSet(),
+
+
+@field:Schema(description = "Total balance for the vendor in each balance display column")
+var vendorTotals: CashFlowBalanceDTO? = null
+
+) {
+   constructor(entity: CashFlowVendorEntity) :
+      this(
+         vendorCompanyId = entity.vendorCompanyId,
+         vendorNumber = entity.vendorNumber,
+         vendorName = entity.vendorName,
+         invoices = entity.invoices,
+         vendorTotals = CashFlowBalanceDTO(entity.vendorTotals)
+      )
+}
