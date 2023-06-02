f6b07701d8df9c805d673357567146fcc251b39d
austine <austine@hightouchinc.com>
2023-06-02 07:38:47 -0600

    Update domain script based on Beckys feedback

--- src/main/resources/db/migration/postgres/V105__feature-create-deposits-staging-deposit-type-domain.sql
+++ src/main/resources/db/migration/postgres/V105__feature-create-deposits-staging-deposit-type-domain.sql
@@ -1,13 +1,8 @@
 CREATE TABLE deposits_staging_deposit_type_domain
 (
-    id                                                   UUID        DEFAULT uuid_generate_v1()                      NOT NULL PRIMARY KEY,
-    value                                                VARCHAR(10)                                                 NOT NULL,
-    description                                          VARCHAR(100)                                                NOT NULL,
-    localization_code                                    VARCHAR(100)                                                NOT NULL
+    id                                                   UUID         DEFAULT uuid_generate_v1()                        NOT NULL PRIMARY KEY,
+    value                                                VARCHAR(10)                                                    NOT NULL,
+    description                                          VARCHAR(100) CHECK ( char_length(trim(description)) > 1)       NOT NULL,
+    localization_code                                    VARCHAR(100) CHECK ( char_length(trim(localization_code)) > 1) NOT NULL,
+    UNIQUE
 );
-
-CREATE TRIGGER update_deposits_staging_deposit_type_domain_trg
-    BEFORE UPDATE
-    ON deposits_staging_deposit_type_domain
-    FOR EACH ROW
-EXECUTE PROCEDURE update_user_table_fn();
