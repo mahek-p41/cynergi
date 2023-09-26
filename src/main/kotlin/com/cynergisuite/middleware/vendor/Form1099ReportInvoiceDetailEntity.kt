c961ab7cc550a93adfd9d9595748737564217717
patricks <patricks@hightouchinc.com>
2023-09-26 14:53:45 -0500

    CYN-1955 WIP
    (cherry picked from commit 6365c1e5b80f9a90d115f13c6d0c282d01d6a572)

---
+++ src/main/kotlin/com/cynergisuite/middleware/vendor/Form1099ReportInvoiceDetailEntity.kt
@@ -0,0 +1,27 @@
+package com.cynergisuite.middleware.vendor
+
+import com.cynergisuite.middleware.accounting.account.payable.AccountPayableInvoiceStatusType
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
+
+data class Form1099ReportInvoiceDetailEntity (
+
+   val invoiceCompanyId: UUID,
+   val invoiceVendorId: UUID,
+   val invoice: String,
+   val invoiceDate: LocalDate,
+   val invoiceAmount: BigDecimal,
+   var invoiceDiscountAmount: BigDecimal?,
+   var invoiceDiscountDate: LocalDate?,
+   val invoiceExpenseDate: LocalDate,
+   var invoicePaidAmount: BigDecimal,
+   var invoiceDiscountTaken: BigDecimal,
+   var invoiceDiscountPercent: BigDecimal?,
+   //var invoiceStatus: AccountPayableInvoiceStatusType,
+   val invoiceDueDate: LocalDate,
+   var discountAmount: BigDecimal?,
+   var lostAmount: BigDecimal?,
+   var balance: BigDecimal,
+   var fieldNumber: Int
+)
--- src/main/kotlin/com/cynergisuite/middleware/vendor/Form1099VendorEntity.kt
+++ src/main/kotlin/com/cynergisuite/middleware/vendor/Form1099VendorEntity.kt
@@ -1,45 +1,24 @@
 package com.cynergisuite.middleware.vendor
 
-import com.cynergisuite.domain.Identifiable
+import com.cynergisuite.middleware.accounting.account.payable.cashflow.CashFlowReportInvoiceDetailEntity
 import com.cynergisuite.middleware.address.AddressEntity
-import com.cynergisuite.middleware.company.CompanyEntity
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
 data class Form1099VendorEntity(
-   val id: UUID? = null,
+   val id: UUID,
    val vendorName: String, // 30 max
    val vendorNumber: Int?,
    val vendorAddress: AddressEntity?,
    val companyAddress: AddressEntity?,
    val federalIdNumber: String?, // 12 max
    val form1099Field: Int,
-   val invoice: String?,
    val apPaymentPaymentDate: LocalDate,
    val accountName: String,
    val accountNumber: String?,
    val distributionAmount: BigDecimal? = null,
-   val isActive: Boolean = true
-) : Identifiable {
-
-
-   constructor(dto: Form1099VendorDTO, company: CompanyEntity) :
-      this(
-         id = dto.id,
-         vendorName = dto.vendorName!!,
-         vendorNumber = dto.vendorNumber!!,
-         vendorAddress = dto.vendorAddress?.let { AddressEntity(it) },
-         companyAddress = company.address,
-         federalIdNumber = dto.federalIdNumber,
-         form1099Field = dto.form1099Field!!,
-         invoice = dto.invoice,
-         apPaymentPaymentDate = dto.apPaymentPaymentDate!!,
-         accountName = dto.accountName!!,
-         accountNumber = dto.accountNumber,
-         distributionAmount = dto.distributionAmount,
-         isActive = dto.isActive
-      )
-
-   override fun myId(): UUID? = id
-}
+   val isActive: Boolean = true,
+   val invoices: MutableSet<CashFlowReportInvoiceDetailEntity>? = LinkedHashSet(),
+   var vendorTotals: Form1099TotalsEntity = Form1099TotalsEntity()
+)
